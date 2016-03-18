7b7f884372cb6f8aaac69ebb9c56d6105c4a160a
Malú <marialourdescentenaro@mbp-de-malu.home>
2016-03-18 00:26:02 +0000

    Adding Jade to the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     gulpWatch = require('gulp-watch'),
     del = require('del'),
-    argv = process.argv;
+    argv = process.argv,
+    jade = require('jade'),
+    gjade = require('gulp-jade');
 
 /**
  * Ionic Gulp tasks, for more information on each see
@@ -15,9 +17,10 @@ var copyFonts = require('ionic-gulp-fonts-copy');
 gulp.task('watch', ['sass', 'html', 'fonts'], function(){
   gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
   gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
+  gulpWatch('app/**/*.jade', function(){ gulp.start('markup'); });
   return buildWebpack({ watch: true });
 });
-gulp.task('build', ['sass', 'html', 'fonts'], buildWebpack);
+gulp.task('build', ['sass', 'markup', 'html', 'fonts'], buildWebpack);
 gulp.task('sass', buildSass);
 gulp.task('html', copyHTML);
 gulp.task('fonts', copyFonts);
@@ -37,3 +40,19 @@ gulp.task('deploy:before', ['build']);
 // we want to 'watch' when livereloading
 var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
 gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
+
+
+
+
+
+
+// I love Jade, so I´ll add it to my Ionic project
+gulp.task('markup', function() {
+  console.log('Converting jade')
+  gulp.src('./app/**/*.jade')
+    .pipe(gjade({
+      jade: jade,
+      pretty: true
+    }))
+    .pipe(gulp.dest('./www/build'));
+});
